139c33ff6de5228eef95d422cc9d5edc43c60233
OmarFawzy74 <106036912+OmarFawzy74@users.noreply.github.com>
2024-04-02 06:29:37 +0200

    my last commit

--- Front-End/src/components/GigCard/GigCard.jsx
+++ Front-End/src/components/GigCard/GigCard.jsx
@@ -4,23 +4,14 @@ import { Link } from "react-router-dom";
 
 const GigCard = ({ item }) => {
   return (
-<<<<<<< HEAD
     <Link to={"/gig/" + item._id} className="link">
-=======
-    <Link to="/gig/123" className="gigLink">
->>>>>>> ed27357f60e5344996d88789ebe56e4edec7ca55
       <div className="gigCard">
         <img className="gigImg" src={item.serviceCover_url} alt="" />
         <div className="gigInfo">
           <div className="user">
-<<<<<<< HEAD
+          <div class="card-badge">WorkHub service</div>
             <img src={item.freelancerId.image_url} alt="" />
             <span>{item.freelancerId.name}</span>
-=======
-          <div class="card-badge">WorkHub service</div>
-            <img src={item.serviceCover_url} alt="" />
-            <span>{item.serviceTitle}</span>
->>>>>>> ed27357f60e5344996d88789ebe56e4edec7ca55
           </div>
           <p>{item.serviceTitle}</p>
           <div className="star">
